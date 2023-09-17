a20dca7efabe0e1677bfdf204b3c3222ea33a398
Dlurak <84224239+Dlurak@users.noreply.github.com>
2023-09-17 17:07:06 +0000

    Improve the design for the share functionality
    Add more emojis

--- src/components/Emoji.tsx
+++ src/components/Emoji.tsx
@@ -12,6 +12,7 @@ const ShareButton = (props: ShareProps) => {
     if (canShare) {
         return (
             <button
+                class="border-l border-slate-300 dark:border-slate-700 pl-2"
                 onClick={() => {
                     navigator.share({
                         text: props.text,
@@ -29,7 +30,7 @@ const ShareButton = (props: ShareProps) => {
 export const Emoji = (props: EmojiProps) => {
     return (
         <button
-            class="flex justify-between gap-2 p-2 rounded-md bg-slate-200 dark:bg-slate-800 hover:bg-slate-300 dark:hover:bg-slate-700"
+            class="flex justify-between gap-2 p-2 rounded-md bg-slate-200 dark:bg-slate-800 hover:bg-slate-300 dark:hover:bg-slate-700 transition-colors duration-300 ease-in-out"
             onClick={() => {
                 navigator.clipboard.writeText(props.emoji);
             }}
