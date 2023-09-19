10449b259045d14995a51072477c16b4d3eb370c
Dlurak <84224239+Dlurak@users.noreply.github.com>
2023-09-19 12:39:15 +0000

    Implement full offline support

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,18 @@
-import { defineConfig } from 'vite'
-import solid from 'vite-plugin-solid'
+import { defineConfig } from 'vite';
+import solid from 'vite-plugin-solid';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [solid()],
-})
+    plugins: [
+        solid(),
+        VitePWA({
+            manifest: {
+                name: 'Emoji',
+                short_name: 'Emoji',
+                theme_color: '#f8fafc',
+                background_color: '#f8fafc',
+                display: 'standalone',
+            },
+        }),
+    ],
+});
