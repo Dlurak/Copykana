f24f39e29c600c48acb54bfec3172bf04790c61f
Dlurak <84224239+Dlurak@users.noreply.github.com>
2023-11-03 20:22:19 +0100

    Optimize the new communication text

--- src/components/emojiPopup.tsx
+++ src/components/emojiPopup.tsx
@@ -1,4 +1,4 @@
-import { Accessor, For, createSignal } from 'solid-js';
+import { Accessor, For, createEffect, createSignal } from 'solid-js';
 import { emojiType, tag, emojiList } from '../constants/emojis';
 import { CopyButton, ShareButton } from './shareButton';
 
@@ -14,6 +14,13 @@ const getTagsByEmoji = (emoji: emojiType): tag[] => {
 export const EmojiPopup = (props: EmojiPopupProps) => {
     const [communicationText, setCommunicationText] = createSignal<string | null>(null)
 
+    createEffect(() => {
+        // just to have some dependency
+        props.emoji()
+
+        setCommunicationText(null)
+    })
+
     return (
         <div class="fixed bottom-2 right-2 bg-slate-200 dark:bg-slate-800 p-4 rounded-md shadow-2xl max-w-full flex flex-col gap-3 items-stretch">
             <div class="flex justify-between items-center gap-2">
@@ -21,17 +28,18 @@ export const EmojiPopup = (props: EmojiPopupProps) => {
                 <button onClick={props.close}>X</button>
             </div>
             <div class="flex gap-2">
-                <CopyButton text={props.emoji()} afterAction={() => setCommunicationText('Copied')} />
+                <CopyButton text={props.emoji()} afterAction={() => {
+                    setCommunicationText('Copied!')
+                    setTimeout(() => setCommunicationText(null), 3000)
+                }} />
                 <ShareButton text={props.emoji()} />
             </div>
             <div class="flex gap-1 flex-wrap">
-                <For each={getTagsByEmoji(props.emoji())}>
-                    {(item) => (
-                        <span class="inline-block bg-slate-300 dark:bg-slate-700 rounded-sm p-2 flex-1">
-                            {item}
-                        </span>
-                    )}
-                </For>
+                <For each={getTagsByEmoji(props.emoji())}>{(item) => (
+                    <span class="inline-block bg-slate-300 dark:bg-slate-700 rounded-sm p-2 flex-1">
+                        {item}
+                    </span>
+                )}</For>
             </div>
             <p class="text-green-800 dark:text-green-500 empty:hidden">{communicationText()}</p>
         </div>
